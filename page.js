f0d7622b74a1696c26089d20405aa7ca3c65b761
Hasan Ali <hasanalismn@gmail.com>
2024-09-30 20:03:35 +0300

    bo

---
+++ page.js
@@ -0,0 +1,16 @@
+// **DOM**
+const monday = document.getElementById("monday");
+const tuesday = document.getElementById("tuesday");
+const wednesday = document.getElementById("wednesday");
+const thursday = document.getElementById("thursday");
+const friday = document.getElementById("friday");
+const saturday = document.getElementById("saturday");
+const sunday = document.getElementById("sunday");
+
+// **FUNCTIONS**
+function Append(){
+
+}
+function Clear(){
+    alert("kahjgsdkjahsdkjh");   
+}
