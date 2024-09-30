6db1083e7082ee0329f9ea91dc0eccd77a8673f1
Hasan Ali <hasanalismn@gmail.com>
2024-09-30 21:53:29 +0300

    Clear Button Works - Centered Append part

--- page.js
+++ page.js
@@ -1,16 +1,12 @@
 // **DOM**
-const monday = document.getElementById("monday");
-const tuesday = document.getElementById("tuesday");
-const wednesday = document.getElementById("wednesday");
-const thursday = document.getElementById("thursday");
-const friday = document.getElementById("friday");
-const saturday = document.getElementById("saturday");
-const sunday = document.getElementById("sunday");
 
 // **FUNCTIONS**
 function Append(){
 
 }
 function Clear(){
-    alert("kahjgsdkjahsdkjh");   
+    txt1.value = '';
+    for (let i = 1; i <= 7; i++) {
+        document.getElementById(`txt${i}`).value = '';
+    }
 }
